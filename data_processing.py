9332fd34351b85f39d61fe003c763c5992cf1ee2
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2022-03-27 17:09:25 +0530

    Completed data-processing block

--- data_processing.py
+++ data_processing.py
@@ -4,7 +4,8 @@
 # Standard library imports
 import os
 import argparse
-from typing import Any, List
+import numpy as np
+from typing import Any, List, Tuple, Union
 
 # Local imports
 # None
@@ -28,12 +29,34 @@ def cli_parser() -> Any:
     )
     parser.add_argument(
         '-O', '--output-dir', action='store', dest='output_dir', type=str,
-        default=None, help="Output directory. If None, a folder named"\
-            "'processed_data' will be created in the input directory"
+        default=None, help="Output directory. If None, a folder named "\
+            "'_processed_data' will be created in the input directory"
     )
 
     args = parser.parse_args()
     return args
+
+def polar2cart(
+    r: Union[float, np.ndarray], theta: Union[float, np.ndarray]
+) -> Union[Tuple[float, float], Tuple[np.ndarray, np.ndarray]]:
+    """
+    Convert polar coordinates to cartesian coordinates.
+
+    Parameters
+    ----------
+    r : float or ndarray
+        Radius.
+    theta : float or ndarray
+        Angle.
+    
+    Returns
+    -------
+    Tuple[float, float] or Tuple[ndarray, ndarray]
+        Cartesian coordinates.
+    """
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    return x, y
 
 class DataProcessor:
     """
@@ -46,46 +69,145 @@ class DataProcessor:
         Input directory which contains all the data.
     input_basefile : str
         Input basefile name.
-    output_dir : str|None
-        Output directory. If None, a folder named 'processed_data' will be
+    output_dir : str
+        Output directory. If None, a folder named '_processed_data' will be
         created in the input directory.
     """
     def __init__(
-        self, input_dir: str, input_basefile: str, output_dir: str|None=None
+        self, input_dir: str, input_basefile: str, output_dir: str=None
     ) -> None:
         self.input_dir = input_dir
         self.input_basefile = input_basefile
-        self.output_dir = output_dir
-        os.ma
-        self.properties = self._get_properties()
-        self.input_files = self._get_input_files()
+        
+        if output_dir is None:
+            self.output_dir = os.path.join(input_dir, "_processed_data")
+            os.makedirs(self.output_dir, exist_ok=True)
+        else:
+            self.output_dir = output_dir
 
+        self.output_fname = os.path.join(
+            self.output_dir, self.input_basefile.replace(".txt", ".npz")
+        )
+        
+        self.properties = self._get_properties()
+
+        self.data:Union[None, dict] = None
+
+    # Private methods
     def _get_properties(self) -> List[str]:
         """
         Get all the properties available in the input directory.
 
-        Returns:
+        Returns
+        -------
             List of properties.
         """
-        properties = os.listdir(self.input_dir)
+        properties = []
+        for prop in os.listdir(self.input_dir):
+            if not prop.startswith("_"):
+                properties.append(prop)
         if properties == []:
             raise ValueError('No properties found in the input directory.')
         return properties
     
-    def _get_input_files(self) -> List[str]:
+    def _get_input_filename(self, prop:str) -> str:
         """
-        Get all the input files available from the input directory, for each
-        of the properties.
+        Get the input filename for the given property.
 
-        Returns:
-            List of input files.
+        Parameters
+        ----------
+        prop : str
+            Property name.
+        
+        Returns
+        -------
+        str
+            Input filename.
         """
-        input_files = []
-        for property in self.properties:
-            input_files.append(
-                os.path.join(self.input_dir, property, self.input_basefile)
+        return os.path.join(self.input_dir, prop, self.input_basefile)
+
+    def _read_data(self, input_file: str) -> List[str]:
+        """
+        Read the data from the given input file.
+
+        Parameters
+        ----------
+        input_file : str
+            Input file path.
+
+        Returns
+        -------
+        List[str]
+            List of lines from the input file.
+        """
+        with open(input_file, "r") as f:
+            lines = f.readlines()
+        return lines
+
+    def _get_simulation_data(self, lines: List[str]) -> List[float]:
+        """
+        Get the simulation data from the input file.
+        Input file needs to have the following data:
+            - Time
+            - R
+            - Theta
+            - Scalar Property
+
+        Parameters
+        ----------
+        lines : List[str]
+            List of lines from the input file.
+
+        Returns
+        -------
+        List[float]
+            Simulation data.
+        """
+        try:
+            time = float(lines[1].split()[2][:-1])
+        except Exception as e:
+            print(
+                "Time not found in the input file. Check the input file's "\
+                "format."
             )
-        return input_files()
+            raise e
+
+        r, theta, scalar_property = [], [], []
+        for i in range(2, len(lines)):
+            data = lines[i].split()
+            r.append(float(data[0]))
+            theta.append(float(data[1]))
+            scalar_property.append(float(data[2]))
+
+        return time, r, theta, scalar_property
+
+    # Public methods
+    def process_data(self) -> None:
+        """
+        Process the data from the input files, and store it as a single file
+        for each property.
+        """
+        data = dict()
+        for prop in self.properties:
+            input_file = self._get_input_filename(prop)
+            lines = self._read_data(input_file)
+            time, r, theta, scalar_property = self._get_simulation_data(lines)
+            
+            if 't' not in data:
+                data['t'], data['r'], data['theta'] = time, r, theta
+            data[prop] = scalar_property
+
+        data['x'], data['y'] = polar2cart(data['r'], data['theta'])
+        self.data = data
+
+    def save_data(self) -> None:
+        """
+        Save the processed data as a single file (format: .npz).
+        """
+        if self.data is None:
+            raise ValueError('No data to save. Run process_data() first.')
+        np.savez(self.output_fname, **self.data)
+        print(f"Saved data to {self.output_fname}")
 
 def main() -> None:
     args = cli_parser()
@@ -94,6 +216,8 @@ def main() -> None:
         input_basefile=args.input_basefile,
         output_dir=args.output_dir
     )
+    data_processor.process_data()
+    data_processor.save_data()
 
 
 ###########################################################################
